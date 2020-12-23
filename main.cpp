1a1bfe7e70e8a5a3635c078e38d7147f5d311cb5
Artur Zając <arturzajac1996@gmail.com>
2020-12-23 19:48:43 +0100

    Added own version  of the convolutio. Modified main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 
-#include "include/FilterExecutor.h"
-#include "include/cudaPtr.h"
-
-
-
+#include "FilterExecutor.h"
+#include "cudaPtr.h"
 
 int main()
 {
-	std::string filename("e:\\studia\\20Z\\RIM\\lena.png");
-	cv::Mat input = cv::imread(filename,cv::IMREAD_COLOR);
+	std::string filename("C:\\lena_std.tif");
+	cv::Mat input = cv::imread(filename, cv::IMREAD_COLOR);
 
-	//FilterExecutor(FilterExecutorMode::CPU).exec(input);
+	auto start = std::chrono::steady_clock::now();
+	FilterExecutor(FilterExecutorMode::CPU).exec(input);
+	auto end = std::chrono::steady_clock::now();
+
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "The filtration took: " << elapsed_seconds.count() << "s time\n";
 
 	cv::imshow(filename, input);
-
 	cv::waitKey(); // unhandled exception
 
-    return 0;
+	return 0;
 }
-
